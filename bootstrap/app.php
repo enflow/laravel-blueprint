1d8a75d5b2e3cfcdc2d5484709768a0df3b88d5a
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2024-03-18 13:31:48 +0100

    Cleanup bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,24 +9,16 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withProviders()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
-        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        // channels: __DIR__.'/../routes/channels.php',
-        health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->redirectGuestsTo(fn () => route('login'));
         $middleware->redirectUsersTo(AppServiceProvider::HOME);
 
         $middleware->web([
-            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
             \Enflow\OutdatedBrowser\OutdatedBrowserMiddleware::class,
         ]);
-
-        $middleware->throttleApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        // $exceptions->dontReport([
-        //     \Symfony\Component\Console\Exception\CommandNotFoundException::class,
-        // ]);
+        //
     })->create();
