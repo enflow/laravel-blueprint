0e7c267131ce09319e8a0b20ceca014d0c27010d
mbardelmeijer <1583095+mbardelmeijer@users.noreply.github.com>
2025-07-18 13:48:12 +0000

    Fix styling

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -28,7 +28,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function dashboards(): array
     {
         return [
-            new \App\Nova\Dashboards\Main(),
+            new \App\Nova\Dashboards\Main,
         ];
     }
 
